231ec5e37d4c57bb9f742eddecfa4ba5acc1089a
Ralphhf <81373119+Ralphhf@users.noreply.github.com>
2023-11-28 00:41:56 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,12 @@ function toggleReadMore(id) {
         content.style.display = "none";
     }
 }
+
+function toggleReadMore(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "none") {
+        content.style.display = "block";
+    } else {
+        content.style.display = "none";
+    }
+}
